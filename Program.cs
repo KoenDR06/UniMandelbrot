6d30a4da37d39d3487a13a3a15b749f3fa2faa00
Maurits Wilke <mauritswilke@gmail.com>
2024-09-26 13:54:21 +0200

    actually removed the gitingored files

--- Program.cs
+++ Program.cs
@@ -1,62 +1,95 @@
 using System.Diagnostics;
-using System.Drawing.Imaging;
 using Mandelbrot;
 
-int resolution = 4096;
+// Settings
+var resolution = 800;
+var maxIterations = 256;
 
-Form screen = new Form();
-screen.ClientSize = new Size(resolution, resolution);
-Label label = new Label();
-screen.Controls.Add(label);
-label.Location = new Point(0, 0);
-label.Size = new Size(resolution, resolution);
 
-string filename = Directory.GetCurrentDirectory() + "..\\..\\..\\..\\presets\\infinite_spiral.mandel";
-// Renderer renderer = new Renderer(resolution, 256, Triangle.RAINBOW_TRIANGLE());
-Renderer renderer = Renderer.ImportMandelbrot(filename, resolution);
+var screen = new Form
+{
+    ClientSize = new Size(resolution + 250, resolution),
+    Text = "Mandelbrot"
+    // BACKLOG: add icon?
+};
 
-void Render() {
-    Stopwatch stopWatch = new Stopwatch();
+// Control panel
+var controlPanel = new FlowLayoutPanel
+{
+    Location = new Point(0, 0),
+    FlowDirection = FlowDirection.TopDown,
+    Dock = DockStyle.Left,
+    Margin = new Padding(0),
+    Padding = new Padding(0),
+    BackColor = Color.Red
+};
+
+
+var mandelbrotImage = new Label
+{
+    Location = new Point(250, 0),
+    Size = new Size(resolution, resolution)
+};
+
+screen.Controls.Add(mandelbrotImage);
+
+var filename = Directory.GetCurrentDirectory() + "..\\..\\..\\..\\presets\\infinite_spiral.mandel";
+var renderer = new Renderer(resolution, maxIterations, Triangle.RAINBOW_TRIANGLE());
+
+void Render()
+{
+    var stopWatch = new Stopwatch();
     stopWatch.Start();
-    label.Image = renderer.RenderMandelbrot();
+    mandelbrotImage.Image = renderer.RenderMandelbrot();
     stopWatch.Stop();
     Console.WriteLine(stopWatch.Elapsed);
 }
 
-void OnScroll(object? o, MouseEventArgs mea) {
-    if (mea.Delta >= 1200) { renderer.MaxIters *= 2; }
-    else if (mea.Delta <= -1200) { renderer.MaxIters /= 2; }
+void OnScroll(object? o, MouseEventArgs mea)
+{
+    if (mea.Delta >= 1200)
+        renderer.MaxIters *= 2;
+    else if (mea.Delta <= -1200)
+        renderer.MaxIters /= 2;
     else renderer.MaxIters += mea.Delta / 12;
-    
+
     if (renderer.MaxIters <= 1) renderer.MaxIters = 1;
 
     Render();
 }
 
-void OnClick(object? o, MouseEventArgs mea) {
+void OnClick(object? o, MouseEventArgs mea)
+{
     renderer.XCenter = Math.Exp(-1.0 * renderer.Zoom) * (4.0 * mea.X / resolution - 2.0) + renderer.XCenter;
     renderer.YCenter = Math.Exp(-1.0 * renderer.Zoom) * (4.0 * mea.Y / resolution - 2.0) + renderer.YCenter;
-    if (mea.Button == MouseButtons.Left) {
+
+    if (mea.Button == MouseButtons.Left)
+    {
         renderer.Zoom += 1;
         renderer.MaxIters += 10;
-    } else if (mea.Button == MouseButtons.Right) {
+    }
+    else if (mea.Button == MouseButtons.Right)
+    {
         renderer.Zoom -= 1;
         renderer.MaxIters -= 10;
     }
+
     Render();
 }
 
 
-label.MouseWheel += OnScroll;
-label.MouseClick += OnClick;
+mandelbrotImage.MouseWheel += OnScroll;
+mandelbrotImage.MouseClick += OnClick;
 
 Render();
 Application.Run(screen);
 
-filename = Directory.GetCurrentDirectory() + "..\\..\\..\\..\\data.mandel";
-renderer.ExportMandelbrot(filename);
-
-label.Image = renderer.RenderMandelbrot();
-
-
-renderer.SaveRenderedImage(Directory.GetCurrentDirectory() + "..\\..\\..\\..\\render.png");
+// BACKLOG: wat doet dit hier?
+//
+// filename = Directory.GetCurrentDirectory() + "..\\..\\..\\..\\data.mandel";
+// renderer.ExportMandelbrot(filename);
+//
+// // mandelbrotImage.Image = renderer.RenderMandelbrot();
+//
+//
+// renderer.SaveRenderedImage(Directory.GetCurrentDirectory() + "..\\..\\..\\..\\render.png");
